5b28d5052f7341462d766127b74b163d4946b358
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-11 00:46:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function driverNamesWithRevenueOver(driver, revenue){
 }
 
 function exactMatch(drivers,attribute){
-  let key = Object.keys(attribute)
-  drivers.filter()
+  let key = Object.keys(attribute)[0]
+  return drivers.filter(function (d) d[key] === attribute[key])
   
 }
